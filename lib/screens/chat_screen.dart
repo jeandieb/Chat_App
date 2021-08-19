f9eade603bc9925a3b5f8d629f09f9156687e04e
jeandieb <johnnydib15@gmail.com>
2021-08-18 22:16:55 -0700

    connected to firebase using cloud firestore

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(itemCount: 15, itemBuilder: (context, index) => Text('hello world'),),
-      
-    );
+      //StreamBuilder is a live listener to changes of values on firebase database,
+      //builder of StreamBuilder takes the code we need to rebuild once a value changes 
+        body: StreamBuilder(
+            stream: Firestore.instance
+                .collection('chats/VcI6P5yUDksHpGUitH3D/messages')
+                .snapshots(),
+            builder: (ctx, streamSnapshot) {
+              if(streamSnapshot.connectionState  == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              }
+              final documents = streamSnapshot.data.documents;
+              return ListView.builder(
+                itemCount: documents.length,
+                itemBuilder: (context, index) => Text(documents[index]['text']),
+              );
+            }));
   }
 }
